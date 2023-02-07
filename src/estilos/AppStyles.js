084bf5491ffd1d75d7e68def2f45306649ad7777
profanapaulacostacurta <123430271+profanapaulacostacurta@users.noreply.github.com>
2023-02-06 23:57:27 -0300

    Create AppStyles.js

---
+++ src/estilos/AppStyles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const AppHeaderStyle = styled.h1` 
+  background-color: #282c34;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+`;
+export const AppStyle = styled.div` 
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;  
+  background-color: #282c34;
+  min-height: 10vh;
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  justify-content: center;
+`;
